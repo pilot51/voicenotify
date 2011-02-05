526ea2e58030ebe4bad400f42c4001797519fbdb
Pilot_51 <pilota51@gmail.com>
2011-02-04 23:06:11 -0500

    Add preference to open Accessibility settings.

--- src/com/pilot51/voicenotify/MainActivity.java
+++ src/com/pilot51/voicenotify/MainActivity.java
@@ -1,12 +1,22 @@
 package com.pilot51.voicenotify;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.Preference.OnPreferenceClickListener;
 
 public class MainActivity extends PreferenceActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
+		
+		((Preference) findPreference("accessibility")).setOnPreferenceClickListener(new OnPreferenceClickListener() {
+			public boolean onPreferenceClick(Preference preference) {
+				startActivity(new Intent(android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS));
+				return true;
+			}
+		});
 	}
 }
