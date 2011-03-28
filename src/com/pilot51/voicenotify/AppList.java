84cc1d1086b9e93844e65a42cf2892041e86fae3
Pilot_51 <pilota51@gmail.com>
2011-03-28 15:45:01 -0400

    Add menu options to ignore all or none.

--- src/com/pilot51/voicenotify/AppList.java
+++ src/com/pilot51/voicenotify/AppList.java
@@ -16,6 +16,8 @@ import android.content.pm.PackageManager;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -29,6 +31,7 @@ public class AppList extends ListActivity {
 	private ProgressDialog progress;
 	private String TAG;
 	private ArrayList<String> ignoredApps;
+	private static final int IGNORE_TOGGLE = 0, IGNORE_ON = 1, IGNORE_OFF = 2;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -73,31 +76,12 @@ public class AppList extends ListActivity {
 					public void run() {
 						final ListView lv = getListView();
 						lv.setTextFilterEnabled(true);
-						adapter = new MySimpleAdapter(AppList.this, appArray, R.layout.app_list_item,
-								new String[] {"label", "package", "enabled"},
-								new int[] {R.id.text1, R.id.text2, R.id.checkbox});
-						lv.setAdapter(adapter);
+						updateList(false);
 						lv.setOnItemClickListener(new OnItemClickListener() {
 							@Override
 							public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-								HashMap<String, String> clickedApp = appArray.get(position);
-								clickedApp.put("enabled", Boolean.toString(!Boolean.parseBoolean(clickedApp.get("enabled"))));
-								appArray.set(position, clickedApp);
-								adapter = new MySimpleAdapter(AppList.this, appArray, R.layout.app_list_item,
-										new String[] {"label", "package", "enabled"},
-										new int[] {R.id.text1, R.id.text2, R.id.checkbox});
-								lv.setAdapter(adapter);
-								String
-									pkg = clickedApp.get("package"),
-									label = clickedApp.get("label");
-								if (ignoredApps.contains(pkg)) {
-									ignoredApps.remove(pkg);
-									Toast.makeText(AppList.this, label + " is not ignored", Toast.LENGTH_SHORT).show();
-								} else {
-									ignoredApps.add(pkg);
-									Toast.makeText(AppList.this, label + " is ignored", Toast.LENGTH_SHORT).show();
-								}
-								saveList(ignoredApps);
+								setIgnore(position, IGNORE_TOGGLE);
+								updateList(true);
 							}
 						});
 						progress.dismiss();
@@ -106,6 +90,59 @@ public class AppList extends ListActivity {
 			}
 		}).start();
 		setVolumeControlStream(AudioManager.STREAM_MUSIC);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		menu.add(0, 1, 0, "Ignore all");
+		menu.add(0, 2, 0, "Ignore none");
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case 1:
+			massIgnore(IGNORE_ON);
+			return true;
+		case 2:
+			massIgnore(IGNORE_OFF);
+			return true;
+		}
+		return false;
+	}
+	
+	private void massIgnore(int ignoreType) {
+		for (int i = 0; i < appArray.size(); i++) {
+			setIgnore(i, ignoreType);
+		}
+		updateList(true);
+	}
+	
+	private void setIgnore(int position, int ignoreType) {
+		HashMap<String, String> app = appArray.get(position);
+		String
+			pkg = app.get("package"),
+			label = app.get("label");
+		if (ignoredApps.contains(pkg) & (ignoreType == IGNORE_TOGGLE | ignoreType == IGNORE_OFF)) {
+			ignoredApps.remove(pkg);
+			app.put("enabled", Boolean.toString(true));
+			if (ignoreType == IGNORE_TOGGLE) Toast.makeText(this, label + " is not ignored", Toast.LENGTH_SHORT).show();
+		} else if (ignoreType == IGNORE_TOGGLE | ignoreType == IGNORE_ON) {
+			ignoredApps.add(pkg);
+			app.put("enabled", Boolean.toString(false));
+			if (ignoreType == IGNORE_TOGGLE) Toast.makeText(this, label + " is ignored", Toast.LENGTH_SHORT).show();
+		}
+		appArray.set(position, app);
+	}
+	
+	private void updateList(boolean doSave) {
+		adapter = new MySimpleAdapter(AppList.this, appArray, R.layout.app_list_item,
+				new String[] {"label", "package", "enabled"},
+				new int[] {R.id.text1, R.id.text2, R.id.checkbox});
+		getListView().setAdapter(adapter);
+		if (doSave) saveList(ignoredApps);
 	}
 
 	private void saveList(ArrayList<String> list) {
