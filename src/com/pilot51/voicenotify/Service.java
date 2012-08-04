707f592e989c3502365a2fdb0ddd154858aebb68
Pilot_51 <pilota51@gmail.com>
2012-08-04 14:39:28 -0400

    Fix ignored strings not working when containing caps.

--- src/com/pilot51/voicenotify/Service.java
+++ src/com/pilot51/voicenotify/Service.java
@@ -100,7 +100,7 @@ public class Service extends AccessibilityService {
 			e.printStackTrace();
 			newMsg = ttsStringPref;
 		}
-		final String[] ignoreStrings = Common.prefs.getString("ignore_strings", "").split("\n");
+		final String[] ignoreStrings = Common.prefs.getString("ignore_strings", "").toLowerCase().split("\n");
 		boolean stringIgnored = false;
 		if (ignoreStrings != null) {
 			for (int i = 0; i < ignoreStrings.length; i++) {
