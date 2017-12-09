pilot51/voicenotify

commit 205ca7bd8610fdde7f67996620bd7f0a94b57037

Add TTS Text Replacement preference.

app/src/main/java/com/pilot51/voicenotify/TextReplacePreference.java
@@ -1,0 +1,424 @@
+/*
+ * Copyright 2017 Mark Injerd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pilot51.voicenotify;
+
+import android.content.Context;
+import android.database.DataSetObserver;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.preference.DialogPreference;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.util.Pair;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Preference that provides a dynamic list with two EditTexts in each row for defining text replacement.
+ */
+public class TextReplacePreference extends DialogPreference {
+	private ListView listView;
+	private TextReplaceAdapter adapter;
+	private boolean isPersistedReplaceListSet;
+	private final List<Pair<String, String>> persistedReplaceList = new ArrayList<>();
+	
+	public TextReplacePreference(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		setDialogLayoutResource(R.layout.preference_dialog_text_replace);
+		setPositiveButtonText(android.R.string.ok);
+		setNegativeButtonText(android.R.string.cancel);
+		listView = new ListView(context, attrs);
+		listView.addOnAttachStateChangeListener(new View.OnAttachStateChangeListener() {
+			@Override
+			public void onViewAttachedToWindow(View v) {
+				Window window = getDialog().getWindow();
+				if (window != null) {
+					// Required for soft keyboard to appear.
+					window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+							| WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
+				} else {
+					Log.e(TextReplacePreference.class.getSimpleName(),
+							"Null window, unable to clear window flags, soft keyboard may not appear.");
+				}
+			}
+			
+			@Override
+			public void onViewDetachedFromWindow(View v) {}
+		});
+	}
+	
+	private void setPersistedReplaceList(List<Pair<String, String>> list) {
+		List<Pair<String, String>> trimmedList = new ArrayList<>(list.size());
+		copyLoop:for (Pair<String, String> pair : list) {
+			if (pair != null && !pair.first.isEmpty()) {
+				for (Pair<String, String> p : trimmedList) {
+					if (pair.first.equalsIgnoreCase(p.first)) {
+						continue copyLoop;
+					}
+				}
+				trimmedList.add(pair);
+			}
+		}
+		boolean changed = false;
+		if (trimmedList.size() != persistedReplaceList.size()) {
+			changed = true;
+		} else {
+			for (int i = 0; i < trimmedList.size(); i++) {
+				if (!trimmedList.get(i).equals(persistedReplaceList.get(i))) {
+					changed = true;
+					break;
+				}
+			}
+		}
+		if (changed || !isPersistedReplaceListSet) {
+			persistedReplaceList.clear();
+			persistedReplaceList.addAll(trimmedList);
+			isPersistedReplaceListSet = true;
+			persistString(convertListToString(persistedReplaceList));
+			if (changed) {
+				notifyDependencyChange(shouldDisableDependents());
+				notifyChanged();
+			}
+		}
+	}
+	
+	private void setPersistedReplaceList(String text) {
+		persistedReplaceList.clear();
+		persistedReplaceList.addAll(convertStringToList(text));
+	}
+	
+	private static String convertListToString(List<Pair<String, String>> list) {
+		StringBuilder saveString = new StringBuilder();
+		for (Pair<String, String> pair : list) {
+			if (pair == null) {
+				break;
+			}
+			if (saveString.length() > 0) {
+				saveString.append("\n");
+			}
+			saveString.append(pair.first);
+			saveString.append("\n");
+			saveString.append(pair.second);
+		}
+		return saveString.toString();
+	}
+	
+	/**
+	 * Converts a string of paired substrings separated by newlines into a list of string pairs.
+	 * @param string The string to convert. Each string in and between pairs must be separated by a newline.
+	 *               There should be an odd number of newlines for an even number of substrings (including zero-length),
+	 *               otherwise the last substring will be discarded.
+	 * @return A List of string pairs.
+	 */
+	static List<Pair<String, String>> convertStringToList(String string) {
+		List<Pair<String, String>> list = new ArrayList<>();
+		if (TextUtils.isEmpty(string)) {
+			return list;
+		}
+		String[] array = string.split("\n", -1);
+		for (int i = 0; i+1 < array.length; i+=2) {
+			list.add(new Pair<>(array[i], array[i+1]));
+		}
+		return list;
+	}
+	
+	@Override
+	protected void onBindDialogView(View view) {
+		super.onBindDialogView(view);
+		adapter = new TextReplaceAdapter();
+		listView.setAdapter(adapter);
+		ViewParent oldParent = listView.getParent();
+		if (oldParent != view) {
+			if (oldParent != null) {
+				((ViewGroup) oldParent).removeView(listView);
+			}
+			ViewGroup container = view.findViewById(R.id.container);
+			if (container != null) {
+				container.addView(listView, ViewGroup.LayoutParams.MATCH_PARENT,
+						ViewGroup.LayoutParams.MATCH_PARENT);
+			}
+		}
+	}
+	
+	@Override
+	protected void onDialogClosed(boolean positiveResult) {
+		if (positiveResult) {
+			adapter.updateFocusedRow();
+			if (callChangeListener(convertListToString(adapter.getList()))) {
+				setPersistedReplaceList(adapter.getList());
+			}
+		}
+	}
+	
+	@Override
+	protected void onSetInitialValue(boolean restorePersistedValue, Object defaultValue) {
+		setPersistedReplaceList(restorePersistedValue ? getPersistedString(convertListToString(persistedReplaceList))
+				: (String)defaultValue);
+	}
+	
+	@Override
+	public boolean shouldDisableDependents() {
+		return persistedReplaceList.isEmpty() || super.shouldDisableDependents();
+	}
+	
+	@Override
+	protected Parcelable onSaveInstanceState() {
+		SavedState myState = new SavedState(super.onSaveInstanceState());
+		adapter.updateFocusedRow();
+		myState.text = convertListToString(adapter.getList());
+		return myState;
+	}
+	
+	@Override
+	protected void onRestoreInstanceState(Parcelable state) {
+		if (state == null || !state.getClass().equals(SavedState.class)) {
+			super.onRestoreInstanceState(state);
+			return;
+		}
+		SavedState myState = (SavedState)state;
+		super.onRestoreInstanceState(myState.getSuperState());
+		adapter.setList(convertStringToList(myState.text));
+	}
+	
+	private static class SavedState extends BaseSavedState {
+		String text;
+		
+		private SavedState(Parcelable superState) {
+			super(superState);
+		}
+		
+		private SavedState(Parcel source) {
+			super(source);
+			text = source.readString();
+		}
+		
+		@Override
+		public void writeToParcel(Parcel dest, int flags) {
+			super.writeToParcel(dest, flags);
+			dest.writeString(text);
+		}
+		
+		public static final Parcelable.Creator<SavedState> CREATOR =
+				new Parcelable.Creator<SavedState>() {
+					public SavedState createFromParcel(Parcel in) {
+						return new SavedState(in);
+					}
+					
+					public SavedState[] newArray(int size) {
+						return new SavedState[size];
+					}
+				};
+	}
+	
+	/**
+	 * Adapter for the list of text replacements.
+	 */
+	// FIXME: Adapter update forces focus to editFrom of the row with focus prior to update.
+	private class TextReplaceAdapter extends BaseAdapter {
+		private final LayoutInflater inflater;
+		private final List<Pair<String, String>> replaceList = new ArrayList<>();
+		
+		private TextReplaceAdapter() {
+			inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			setList(persistedReplaceList);
+		}
+		
+		private List<Pair<String, String>> getList() {
+			return replaceList;
+		}
+		
+		/**
+		 * Sets the adapter data and adds null for an empty last row
+		 * if the list doesn't already contain null (it shouldn't).
+		 * @param list The data to set in the adapter.
+		 */
+		private void setList(List<Pair<String, String>> list) {
+			replaceList.clear();
+			replaceList.addAll(list);
+			if (!replaceList.contains(null)) {
+				replaceList.add(null);
+			}
+			notifyDataSetChanged();
+		}
+		
+		/**
+		 * Finds the focused row and forces the entered text to be updated in the adapter's data.
+		 * This is used when the dialog's positive button is pressed or when saving instance state.
+		 */
+		private void updateFocusedRow() {
+			View focusedRow = listView.getFocusedChild();
+			if (focusedRow != null) {
+				updateListItem((ViewHolder)focusedRow.getTag());
+			}
+		}
+		
+		/**
+		 * Updates a row in the adapter's data.
+		 * @param holder The ViewHolder for the row. The EditTexts and position in data are used.
+		 */
+		private void updateListItem(ViewHolder holder) {
+			Pair<String, String> listPair = null;
+			if (holder.position < replaceList.size()) {
+				listPair = replaceList.get(holder.position);
+			}
+			Pair<String, String> editStringPair = Pair.create(holder.editFrom.getText().toString(),
+					holder.editTo.getText().toString());
+			if (!editStringPair.first.isEmpty()) {
+				if (listPair == null) {
+					replaceList.add(holder.position, editStringPair);
+					notifyDataSetChanged();
+				} else if (!editStringPair.first.equals(listPair.first)
+						|| !editStringPair.second.equals(listPair.second)) {
+					replaceList.set(holder.position, editStringPair);
+				}
+			} else if (listPair != null) {
+				replaceList.remove(holder.position);
+				notifyDataSetChanged();
+			}
+		}
+		
+		/**
+		 * Sets EditText error message if entered text is a duplicate of another entry in {@link #replaceList},
+		 * otherwise clears error if not a duplicate.
+		 * @param editFrom The EditText to check and set error message if text is duplicate, otherwise clear error.
+		 * @param position The position of this entry in {@link #replaceList} to prevent detecting self as duplicate.
+		 */
+		private void setErrorIfDuplicate(EditText editFrom, int position) {
+			String editFromString = editFrom.getText().toString();
+			for (Pair<String, String> p : replaceList) {
+				if (p != null && replaceList.indexOf(p) < position && editFromString.equalsIgnoreCase(p.first)) {
+					editFrom.setError(getContext().getString(R.string.text_replace_error_duplicate));
+					return;
+				}
+			}
+			editFrom.setError(null);
+		}
+		
+		@Override
+		public int getCount() {
+			return replaceList.size();
+		}
+		
+		@Override
+		public Object getItem(int position) {
+			return replaceList.get(position);
+		}
+		
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+		
+		private class ViewHolder {
+			private int position;
+			private EditText editFrom;
+			private EditText editTo;
+			private ImageButton remove;
+		}
+		
+		@Override
+		public View getView(final int position, View convertView, ViewGroup parent) {
+			final ViewHolder holder;
+			if (convertView == null) {
+				convertView = inflater.inflate(R.layout.text_replace_row, parent, false);
+				holder = new ViewHolder();
+				holder.editFrom = convertView.findViewById(R.id.text_to_replace);
+				holder.editTo = convertView.findViewById(R.id.replacement_text);
+				holder.remove = convertView.findViewById(R.id.remove);
+				convertView.setTag(holder);
+			} else {
+				holder = (ViewHolder)convertView.getTag();
+			}
+			holder.position = position;
+			final Pair<String, String> pair = replaceList.get(position);
+			final EditText editFrom = holder.editFrom;
+			final EditText editTo = holder.editTo;
+			holder.remove.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					editFrom.setText(null);
+					editTo.setText(null);
+					updateListItem(holder);
+				}
+			});
+			if (pair != null) {
+				editFrom.setText(pair.first);
+				editTo.setText(pair.second);
+			} else {
+				editFrom.setText(null);
+				editTo.setText(null);
+			}
+			setErrorIfDuplicate(editFrom, position);
+			final DataChangedObserver observer = new DataChangedObserver();
+			registerDataSetObserver(observer);
+			View.OnFocusChangeListener listener = new View.OnFocusChangeListener() {
+				@Override
+				public void onFocusChange(View v, boolean hasFocus) {
+					if (hasFocus || observer.dataChanged) return;
+					setErrorIfDuplicate(editFrom, position);
+					updateListItem(holder);
+				}
+			};
+			editFrom.setOnFocusChangeListener(listener);
+			editTo.setOnFocusChangeListener(listener);
+			editTo.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+				@Override
+				public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+					if (actionId == EditorInfo.IME_ACTION_DONE) {
+						if (position != replaceList.size() - 1) {
+							TextView nextField = (TextView)editFrom.focusSearch(View.FOCUS_DOWN);
+							if (nextField != null) {
+								nextField.requestFocus();
+							}
+						}
+						return true;
+					}
+					return false;
+				}
+			});
+			return convertView;
+		}
+		
+		/**
+		 * A DataSetObserver used to prevent onFocusChange of the EditTexts from erroneously updating
+		 * data with old row information after data has changed from onClick of the remove button.
+		 */
+		private class DataChangedObserver extends DataSetObserver {
+			private boolean dataChanged;
+			@Override
+			public void onChanged() {
+				dataChanged = true;
+				unregisterDataSetObserver(this);
+			}
+		}
+	}
+}

app/src/main/java/com/pilot51/voicenotify/Service.java
@@ -23,11 +23,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.PowerManager;
+import android.preference.PreferenceManager;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.speech.tts.TextToSpeech;
@@ -35,6 +37,7 @@ import android.speech.tts.UtteranceProgressListener;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.Log;
+import android.util.Pair;
 import android.widget.Toast;
 
 import java.text.MessageFormat;
@@ -42,13 +45,16 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.IllegalFormatException;
+import java.util.List;
 import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.regex.Pattern;
 
 public class Service extends NotificationListenerService {
 	private static final String TAG = Service.class.getSimpleName();
+	private SharedPreferences prefs;
 	private final Map<App, String> lastMsg = new HashMap<>();
 	private final Map<App, Long> lastMsgTime = new HashMap<>();
 	private TextToSpeech mTts;
@@ -80,6 +86,7 @@ public class Service extends NotificationListenerService {
 
 	@Override
 	public void onCreate() {
+		prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 		mTts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
 			@Override
 			public void onInit(int status) {
@@ -127,7 +134,7 @@ public class Service extends NotificationListenerService {
 		@SuppressLint("InlinedApi") String contentTitle = extras.getString(Notification.EXTRA_TITLE);
 		@SuppressLint("InlinedApi") String contentText = extras.getString(Notification.EXTRA_TEXT);
 		@SuppressLint("InlinedApi") String contentInfoText = extras.getString(Notification.EXTRA_INFO_TEXT);
-		final String ttsStringPref = Common.getPrefs(this).getString(getString(R.string.key_ttsString), "");
+		final String ttsStringPref = prefs.getString(getString(R.string.key_ttsString), "");
 		App app = AppList.findOrAddApp(sbn.getPackageName(), this);
 		String ttsUnformattedMsg = ttsStringPref
 				.replace("#a", "%1$s") // App Label
@@ -152,6 +159,13 @@ public class Service extends NotificationListenerService {
 		if (app != null && (ttsMsg == null || ttsMsg.equals(app.getLabel()))) {
 			ttsMsg = getString(R.string.notification_from, app.getLabel());
 		}
+		if (!TextUtils.isEmpty(ttsMsg)) {
+			String ttsTextReplace = prefs.getString(getString(R.string.key_ttsTextReplace), null);
+			List<Pair<String, String>> textReplaceList = TextReplacePreference.convertStringToList(ttsTextReplace);
+			for (Pair<String, String> pair : textReplaceList) {
+				ttsMsg = ttsMsg.replaceAll("(?i)" + Pattern.quote(pair.first), pair.second);
+			}
+		}
 		StringBuilder logBuilder = new StringBuilder();
 		for (String s : new String[] {ticker, subtext, contentTitle, contentText, contentInfoText}) {
 			if (!TextUtils.isEmpty(s)) {
@@ -160,7 +174,7 @@ public class Service extends NotificationListenerService {
 			}
 		}
 		NotifyList.addNotification(app, logBuilder.toString());
-		final String[] ignoreStrings = Common.getPrefs(this).getString(getString(R.string.key_ignore_strings), "").toLowerCase().split("\n");
+		final String[] ignoreStrings = prefs.getString(getString(R.string.key_ignore_strings), "").toLowerCase().split("\n");
 		boolean stringIgnored = false;
 		for (String s : ignoreStrings) {
 			if (s.length() != 0 && ttsMsg != null && ttsMsg.toLowerCase().contains(s)) {
@@ -178,19 +192,19 @@ public class Service extends NotificationListenerService {
 			ignoreReasons.add(getString(R.string.reason_string));
 		}
 		if (TextUtils.isEmpty(ttsMsg)
-				&& Common.getPrefs(this).getBoolean(getString(R.string.key_ignore_empty), false)) {
+				&& prefs.getBoolean(getString(R.string.key_ignore_empty), false)) {
 			ignoreReasons.add(getString(R.string.reason_empty_msg));
 		}
-		int ignoreRepeat = Integer.parseInt(Common.getPrefs(this).getString(getString(R.string.key_ignore_repeat), "-1"));
+		int ignoreRepeat = Integer.parseInt(prefs.getString(getString(R.string.key_ignore_repeat), "-1"));
 		if (lastMsg.containsKey(app)) {
 			if (lastMsg.get(app).equals(ttsMsg) && (ignoreRepeat == -1 || msgTime - lastMsgTime.get(app) < ignoreRepeat * 1000)) {
 				ignoreReasons.add(MessageFormat.format(getString(R.string.reason_identical), ignoreRepeat));
 			}
 		}
 		if (ignoreReasons.isEmpty()) {
-			int delay = Integer.parseInt(Common.getPrefs(this).getString(getString(R.string.key_ttsDelay), "0"));
+			int delay = Integer.parseInt(prefs.getString(getString(R.string.key_ttsDelay), "0"));
 			if (!isScreenOn()) {
-				int interval = Integer.parseInt(Common.getPrefs(this).getString(getString(R.string.key_tts_repeat), "0"));
+				int interval = Integer.parseInt(prefs.getString(getString(R.string.key_tts_repeat), "0"));
 				if (interval > 0) {
 					repeatList.add(ttsMsg);
 					if (repeater == null) {
@@ -201,7 +215,7 @@ public class Service extends NotificationListenerService {
 			if (delay < 0) { // Just in case we get a weird value, don't want to try to make the Timer wait for negative time
 				delay = 0;
 			}
-			int maxLength = Integer.parseInt(Common.getPrefs(this).getString(getString(R.string.key_max_length), "0"));
+			int maxLength = Integer.parseInt(prefs.getString(getString(R.string.key_max_length), "0"));
 			final String msg;
 			if (ttsMsg != null && maxLength > 0) {
 				msg = ttsMsg.substring(0, Math.min(maxLength, ttsMsg.length()));
@@ -274,30 +288,30 @@ public class Service extends NotificationListenerService {
 	private boolean ignore(boolean isNew) {
 		Calendar c = Calendar.getInstance();
 		int calTime = c.get(Calendar.HOUR_OF_DAY) * 60 + c.get(Calendar.MINUTE),
-			quietStart = Common.getPrefs(this).getInt(getString(R.string.key_quietStart), 0),
-			quietEnd = Common.getPrefs(this).getInt(getString(R.string.key_quietEnd), 0);
+			quietStart = prefs.getInt(getString(R.string.key_quietStart), 0),
+			quietEnd = prefs.getInt(getString(R.string.key_quietEnd), 0);
 		if ((quietStart < quietEnd & quietStart <= calTime & calTime < quietEnd)
 				|| (quietEnd < quietStart && (quietStart <= calTime || calTime < quietEnd))) {
 			ignoreReasons.add(getString(R.string.reason_quiet));
 		}
 		if ((audioMan.getRingerMode() == AudioManager.RINGER_MODE_SILENT
 				|| audioMan.getRingerMode() == AudioManager.RINGER_MODE_VIBRATE)
-				&& !Common.getPrefs(this).getBoolean(Common.KEY_SPEAK_SILENT_ON, false)) {
+				&& !prefs.getBoolean(Common.KEY_SPEAK_SILENT_ON, false)) {
 			ignoreReasons.add(getString(R.string.reason_silent));
 		}
 		if (telephony.getCallState() != TelephonyManager.CALL_STATE_IDLE) {
 			ignoreReasons.add(getString(R.string.reason_call));
 		}
-		if (!isScreenOn() && !Common.getPrefs(this).getBoolean(Common.KEY_SPEAK_SCREEN_OFF, true)) {
+		if (!isScreenOn() && !prefs.getBoolean(Common.KEY_SPEAK_SCREEN_OFF, true)) {
 			ignoreReasons.add(getString(R.string.reason_screen_off));
 		}
-		if (isScreenOn() && !Common.getPrefs(this).getBoolean(Common.KEY_SPEAK_SCREEN_ON, true)) {
+		if (isScreenOn() && !prefs.getBoolean(Common.KEY_SPEAK_SCREEN_ON, true)) {
 			ignoreReasons.add(getString(R.string.reason_screen_on));
 		}
-		if (!isHeadsetOn() && !Common.getPrefs(this).getBoolean(Common.KEY_SPEAK_HEADSET_OFF, true)) {
+		if (!isHeadsetOn() && !prefs.getBoolean(Common.KEY_SPEAK_HEADSET_OFF, true)) {
 			ignoreReasons.add(getString(R.string.reason_headset_off));
 		}
-		if (isHeadsetOn() && !Common.getPrefs(this).getBoolean(Common.KEY_SPEAK_HEADSET_ON, true)) {
+		if (isHeadsetOn() && !prefs.getBoolean(Common.KEY_SPEAK_HEADSET_ON, true)) {
 			ignoreReasons.add(getString(R.string.reason_headset_on));
 		}
 		if (!ignoreReasons.isEmpty()) {

app/src/main/res/layout/preference_dialog_text_replace.xml
@@ -1,0 +1,19 @@
+<?xml version="1.0" encoding="utf-8"?>
+<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
+              android:id="@+id/container"
+              android:layout_width="match_parent"
+              android:layout_height="match_parent"
+              android:layout_marginTop="48dp"
+              android:layout_marginBottom="48dp"
+              android:padding="20dp"
+              android:orientation="vertical">
+	<TextView
+		android:id="@android:id/message"
+		android:layout_width="match_parent"
+		android:layout_height="wrap_content"
+		android:layout_marginBottom="48dp"
+		android:paddingStart="4dp"
+		android:paddingEnd="4dp"
+		android:textColor="?android:attr/textColorSecondary"
+		style="?android:attr/textAppearanceSmall" />
+</LinearLayout>

app/src/main/res/layout/text_replace_row.xml
@@ -1,0 +1,29 @@
+<?xml version="1.0" encoding="utf-8"?>
+<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
+              android:orientation="horizontal"
+              android:layout_width="match_parent"
+              android:layout_height="wrap_content">
+	<EditText
+		android:id="@+id/text_to_replace"
+		android:layout_width="match_parent"
+		android:layout_height="wrap_content"
+		android:layout_weight="1"
+		android:hint="@string/text_to_replace"
+		android:inputType="textShortMessage"
+		android:imeOptions="actionNext" />
+	<EditText
+		android:id="@+id/replacement_text"
+		android:layout_width="match_parent"
+		android:layout_height="wrap_content"
+		android:layout_weight="1"
+		android:hint="@string/replacement_text"
+		android:inputType="textShortMessage"
+		android:imeOptions="actionDone" />
+	<ImageButton
+		android:id="@+id/remove"
+		android:layout_width="wrap_content"
+		android:layout_height="wrap_content"
+		android:layout_gravity="center"
+		android:background="@null"
+		android:src="@android:drawable/ic_menu_close_clear_cancel" />
+</LinearLayout>

app/src/main/res/values-v14/themes.xml
@@ -1,4 +1,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<resources>
-    <style name="AppTheme" parent="android:Theme.DeviceDefault"/>
-</resources>

app/src/main/res/values/donottranslate.xml
@@ -21,6 +21,7 @@
 	<string name="key_appList">appList</string>
 	<string name="key_ttsSettings">ttsSettings</string>
 	<string name="key_ttsString">ttsString</string>
+	<string name="key_ttsTextReplace">ttsTextReplace</string>
 	<string name="key_ttsStream">ttsStream</string>
 	<string name="key_ttsDelay">ttsDelay</string>
 	<string name="key_tts_repeat">tts_repeat</string>

app/src/main/res/values/strings.xml
@@ -22,9 +22,6 @@
 	<string name="service_running">VN service is running</string>
 	<string name="service_suspended">VN service is suspended</string>
 	
-	<!-- xml/service_config.xml -->
-	<string name="service_desc">Reads status bar notifications with Text-To-Speech.</string>
-	
 	<!-- xml/preferences.xml -->
 	<string name="app_list">App List</string>
 	<string name="app_list_summary">List installed apps which can be ignored</string>
@@ -36,6 +33,9 @@
 	<string name="tts_message">TTS Message</string>
 	<string name="tts_message_summary">Customize how Text-To-Speech says notifications</string>
 	<string name="tts_message_dialog">#a = App title\n#t = Ticker\n#s = Subtext\n#c = Content title\n#m = Content message\n#i = Content info\n\nDefault:\n#a. #c. #m.</string>
+	<string name="tts_text_replace">TTS Text Replacement</string>
+	<string name="tts_text_replace_summary">Customize how Text-To-Speech speaks</string>
+	<string name="tts_text_replace_dialog">Substitute text to be spoken, allowing you to customize how Text-To-Speech pronounces words or replace text for any other reason.\n\nText to replace is case insensitive.</string>
 	<string name="tts_stream">TTS Audio Stream</string>
 	<string name="tts_stream_summary">Choose the audio stream that Text-To-Speech plays through</string>
 	<string name="tts_delay">TTS Delay</string>
@@ -86,6 +86,13 @@
 	<string name="test_ignored">Voice Notify is ignored in App List.\nTest will run, but notification will not be spoken.</string>
 	<string name="notify_log_summary">List last 20 notifications.\nDisplays time, app name, message, and ignore reasons.</string>
 	<string name="support_summary">Donate, rate/comment, translations, source code, or contact the developer</string>
+	
+	<!-- TextReplacePreference.java -->
+	<string name="text_replace_error_duplicate">Duplicate! Will not be saved.</string>
+	
+	<!-- text_replace_row.xml -->
+	<string name="text_to_replace">Text to replace</string>
+	<string name="replacement_text">Replacement text</string>
 	
 	<!-- MainActivity.java and xml/preferences.xml -->
 	<string name="test">Test</string>

app/src/main/res/values/themes.xml
@@ -1,0 +1,4 @@
+<?xml version="1.0" encoding="utf-8"?>
+<resources>
+    <style name="AppTheme" parent="android:Theme.DeviceDefault" />
+</resources>

commit adf55291812c0d18337bd679220fd8d5b709585e

Fix NPE when saving instance state.
Only happens when TextReplacePreference dialog isn't shown. Whoops.
Regression in previous commit (205ca7bd).

app/src/main/java/com/pilot51/voicenotify/TextReplacePreference.java
@@ -192,7 +192,11 @@ public class TextReplacePreference extends DialogPreference {
 	
 	@Override
 	protected Parcelable onSaveInstanceState() {
-		SavedState myState = new SavedState(super.onSaveInstanceState());
+		Parcelable superState = super.onSaveInstanceState();
+		if (getDialog() == null || !getDialog().isShowing()) {
+			return superState;
+		}
+		SavedState myState = new SavedState(superState);
 		adapter.updateFocusedRow();
 		myState.text = convertListToString(adapter.getList());
 		return myState;