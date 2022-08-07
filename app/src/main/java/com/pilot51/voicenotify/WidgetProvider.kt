02cac451b39b81b433e9d042007cc47271d37e3d
Mark Injerd <pilota51@gmail.com>
2022-08-06 22:09:24 -0400

    Fix crash at launch on Android 12+
    - Mutability flag required for PendingIntent in WidgetProvider

--- app/src/main/java/com/pilot51/voicenotify/WidgetProvider.kt
+++ app/src/main/java/com/pilot51/voicenotify/WidgetProvider.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Mark Injerd
+ * Copyright 2013-2022 Mark Injerd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,7 @@ import android.appwidget.AppWidgetProvider
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.widget.RemoteViews
 import android.widget.Toast
 import com.pilot51.voicenotify.Common.notificationListenerSettingsIntent
@@ -29,7 +30,11 @@ import com.pilot51.voicenotify.Service.Companion.isSuspended
 import com.pilot51.voicenotify.Service.Companion.toggleSuspend
 
 class WidgetProvider : AppWidgetProvider() {
-	override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+	override fun onUpdate(
+		context: Context,
+		appWidgetManager: AppWidgetManager,
+		appWidgetIds: IntArray
+	) {
 		for (appWidgetId in appWidgetIds) {
 			val views = RemoteViews(context.packageName, R.layout.appwidget)
 			updateViews(context, views)
@@ -40,25 +45,39 @@ class WidgetProvider : AppWidgetProvider() {
 	override fun onReceive(context: Context, intent: Intent) {
 		when (intent.action!!) {
 			ACTION_TOGGLE -> if (isRunning) {
-				Toast.makeText(context,
-					if (toggleSuspend()) R.string.service_suspended else R.string.service_running,
-					Toast.LENGTH_SHORT).show()
+				Toast.makeText(
+					context,
+					if (toggleSuspend()) {
+						R.string.service_suspended
+					} else R.string.service_running,
+					Toast.LENGTH_SHORT
+				).show()
 			}
 			ACTION_UPDATE -> {
 				val views = RemoteViews(context.packageName, R.layout.appwidget)
 				updateViews(context, views)
-				AppWidgetManager.getInstance(context).updateAppWidget(ComponentName(context, WidgetProvider::class.java), views)
+				AppWidgetManager.getInstance(context).updateAppWidget(
+					ComponentName(context, WidgetProvider::class.java), views
+				)
 			}
 			ACTION_ON -> if (isRunning) {
-				Toast.makeText(context,
-					if (toggleSuspend(false)) R.string.service_suspended else R.string.service_running,
-					Toast.LENGTH_SHORT).show()
+				Toast.makeText(
+					context,
+					if (toggleSuspend(false)) {
+						R.string.service_suspended
+					} else R.string.service_running,
+					Toast.LENGTH_SHORT
+				).show()
 			}
 			ACTION_OFF -> {
 				if (isRunning) {
-					Toast.makeText(context,
-						if (toggleSuspend(true)) R.string.service_suspended else R.string.service_running,
-						Toast.LENGTH_SHORT).show()
+					Toast.makeText(
+						context,
+						if (toggleSuspend(true)) {
+							R.string.service_suspended
+						} else R.string.service_running,
+						Toast.LENGTH_SHORT
+					).show()
 				}
 				super.onReceive(context, intent)
 			}
@@ -74,15 +93,27 @@ class WidgetProvider : AppWidgetProvider() {
 		private fun updateViews(context: Context, views: RemoteViews) {
 			val pendingIntent: PendingIntent
 			if (isRunning) {
-				pendingIntent = PendingIntent.getBroadcast(context, 0,
-					Intent(context, WidgetProvider::class.java).setAction(ACTION_TOGGLE), 0)
-				if (isSuspended) {
-					views.setImageViewResource(R.id.button, R.drawable.widget_suspended)
-				} else {
-					views.setImageViewResource(R.id.button, R.drawable.widget_running)
-				}
+				pendingIntent = PendingIntent.getBroadcast(
+					context, 0,
+					Intent(context, WidgetProvider::class.java).setAction(ACTION_TOGGLE),
+					if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+						PendingIntent.FLAG_IMMUTABLE
+					} else 0
+				)
+				views.setImageViewResource(
+					R.id.button,
+					if (isSuspended) {
+						R.drawable.widget_suspended
+					} else R.drawable.widget_running
+				)
 			} else {
-				pendingIntent = PendingIntent.getActivity(context, 0, notificationListenerSettingsIntent, 0)
+				pendingIntent = PendingIntent.getActivity(
+					context, 0,
+					notificationListenerSettingsIntent,
+					if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+						PendingIntent.FLAG_IMMUTABLE
+					} else 0
+				)
 				views.setImageViewResource(R.id.button, R.drawable.widget_disabled)
 			}
 			views.setOnClickPendingIntent(R.id.button, pendingIntent)
