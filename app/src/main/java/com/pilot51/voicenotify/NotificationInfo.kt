pilot51/voicenotify

commit 870b1a3e04382606f8e414b0b9c830767dda620b

Resolve deprecations
Check/request READ_PHONE_STATE permission at launch.
Fix text replace regex not working. Regression: 02caa628
General code improvements.

app/src/main/java/com/pilot51/voicenotify/NotificationInfo.kt
@@ -18,9 +18,9 @@ package com.pilot51.voicenotify
 import android.annotation.SuppressLint
 import android.app.Notification
 import android.content.Context
-import android.preference.PreferenceManager
 import android.text.TextUtils
 import android.util.Log
+import androidx.preference.PreferenceManager
 import com.pilot51.voicenotify.IgnoreReason.Companion.convertSetToString
 import java.text.MessageFormat
 import java.text.SimpleDateFormat
@@ -42,7 +42,7 @@ class NotificationInfo @SuppressLint("InlinedApi") constructor(
 	context: Context
 ) {
 	/** The notification's ticker message. */
-	private val ticker: String? = if (notification.tickerText != null) notification.tickerText.toString() else null
+	private val ticker = notification.tickerText?.toString()
 	/** The notification's subtext. */
 	private val subtext: String?
 	/** The notification's content title. */
@@ -108,7 +108,7 @@ class NotificationInfo @SuppressLint("InlinedApi") constructor(
 			val ttsTextReplace = prefs.getString(context.getString(R.string.key_ttsTextReplace), null)
 			val textReplaceList = TextReplacePreference.convertStringToList(ttsTextReplace)
 			for (pair in textReplaceList) {
-				ttsMessage = ttsMessage!!.replace("(?i)" + Pattern.quote(pair.first).toRegex(), pair.second)
+				ttsMessage = ttsMessage!!.replace("(?i)${Pattern.quote(pair.first)}".toRegex(), pair.second)
 			}
 		}
 		if (ttsMessage != null) {

app/src/main/java/com/pilot51/voicenotify/Service.kt
@@ -15,25 +15,33 @@
  */
 package com.pilot51.voicenotify
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Notification
 import android.bluetooth.BluetoothDevice
 import android.content.*
+import android.content.pm.PackageManager
+import android.media.AudioAttributes
+import android.media.AudioDeviceInfo
+import android.media.AudioFocusRequest
 import android.media.AudioManager
-import android.os.Handler
+import android.os.Build
+import android.os.Bundle
 import android.os.IBinder
 import android.os.PowerManager
-import android.preference.PreferenceManager
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 import android.speech.tts.TextToSpeech
 import android.speech.tts.TextToSpeech.OnInitListener
 import android.speech.tts.UtteranceProgressListener
+import android.telecom.TelecomManager
 import android.telephony.TelephonyManager
 import android.text.TextUtils
 import android.util.Log
 import android.widget.Toast
-import com.pilot51.voicenotify.AppListActivity.Companion.findOrAddApp
+import androidx.core.app.ActivityCompat
+import androidx.preference.PreferenceManager
+import com.pilot51.voicenotify.AppListFragment.Companion.findOrAddApp
 import com.pilot51.voicenotify.Common.getPrefs
 import com.pilot51.voicenotify.Common.getSelectedAudioStream
 import com.pilot51.voicenotify.Common.init
@@ -41,20 +49,29 @@ import com.pilot51.voicenotify.IgnoreReason.Companion.convertSetToString
 import com.pilot51.voicenotify.NotifyList.Companion.addNotification
 import com.pilot51.voicenotify.NotifyList.Companion.refresh
 import com.pilot51.voicenotify.Shake.OnShakeListener
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.util.*
 
 class Service : NotificationListenerService() {
-	private var prefs: SharedPreferences? = null
+	private lateinit var prefs: SharedPreferences
 	private val lastMsg: MutableMap<App?, String?> = HashMap()
 	private val lastMsgTime: MutableMap<App?, Long> = HashMap()
-	private var mTts: TextToSpeech? = null
+	private var tts: TextToSpeech? = null
 	private var shouldRequestFocus = false
-	private var audioMan: AudioManager? = null
-	private var telephony: TelephonyManager? = null
+	private lateinit var audioMan: AudioManager
+	private lateinit var telephony: TelephonyManager
 	private val stateReceiver = DeviceStateReceiver()
 	private var repeater: RepeatTimer? = null
-	private var shake: Shake? = null
+	private lateinit var shake: Shake
 	private val repeatList: MutableList<NotificationInfo> = ArrayList()
+	private val audioFocusRequest = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+		AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK)
+			.setAudioAttributes(AudioAttributes.Builder()
+				.setLegacyStreamType(AudioManager.STREAM_MUSIC).build())
+			.build()
+	} else null
 
 	/**
 	 * this is used to determine if we are the first, middle, or last thing to be spoken at the moment, for enabling/disabling shake and audio focus request
@@ -64,16 +81,15 @@ class Service : NotificationListenerService() {
 	 */
 	@SuppressLint("UseSparseArrays")
 	private val ttsQueue: MutableMap<Long, NotificationInfo> = HashMap()
-	private val handler = Handler()
-	private val statusListener: OnStatusChangeListener = object : OnStatusChangeListener {
+	private val statusListener = object : OnStatusChangeListener {
 		override fun onStatusChanged() {
-			if (isSuspended && mTts != null) {
+			if (isSuspended && tts != null) {
 				synchronized(ttsQueue) {
 					for (info in ttsQueue.values) {
 						info.addIgnoreReason(IgnoreReason.SUSPENDED)
 					}
 				}
-				mTts!!.stop()
+				tts!!.stop()
 			}
 			sendBroadcast(Intent(applicationContext, WidgetProvider::class.java)
 				.setAction(WidgetProvider.ACTION_UPDATE))
@@ -82,13 +98,13 @@ class Service : NotificationListenerService() {
 
 	override fun onCreate() {
 		prefs = PreferenceManager.getDefaultSharedPreferences(applicationContext)
-		mTts = TextToSpeech(applicationContext, OnInitListener { status ->
+		tts = TextToSpeech(applicationContext, OnInitListener { status ->
 			if (status == TextToSpeech.ERROR) {
 				Log.w(TAG, getString(R.string.error_tts_init))
 				Toast.makeText(applicationContext, R.string.error_tts_init, Toast.LENGTH_LONG).show()
 				return@OnInitListener
 			}
-			mTts!!.setOnUtteranceProgressListener(object : UtteranceProgressListener() {
+			tts!!.setOnUtteranceProgressListener(object : UtteranceProgressListener() {
 				override fun onStart(utteranceId: String) {}
 				override fun onStop(utteranceId: String, interrupted: Boolean) {
 					if (interrupted) {
@@ -106,9 +122,14 @@ class Service : NotificationListenerService() {
 					synchronized(ttsQueue) { ttsQueue.remove(utteranceId.toLong()) }
 					if (ttsQueue.isEmpty()) {
 						if (shouldRequestFocus) {
-							audioMan!!.abandonAudioFocus(null)
+							if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+								audioMan.abandonAudioFocusRequest(audioFocusRequest!!)
+							} else {
+								@Suppress("DEPRECATION")
+								audioMan.abandonAudioFocus(null)
+							}
 						}
-						shake!!.disable()
+						shake.disable()
 					}
 				}
 
@@ -122,7 +143,7 @@ class Service : NotificationListenerService() {
 
 	override fun onNotificationPosted(sbn: StatusBarNotification) {
 		val notification = sbn.notification
-		if (prefs!!.getBoolean(getString(R.string.key_ignore_groups), true)
+		if (prefs.getBoolean(getString(R.string.key_ignore_groups), true)
 			&& notification.flags and Notification.FLAG_GROUP_SUMMARY != 0) {
 			return  // Completely ignore group summary notifications.
 		}
@@ -130,7 +151,7 @@ class Service : NotificationListenerService() {
 		val info = NotificationInfo(app, notification, applicationContext)
 		val msgTime = info.calendar.timeInMillis
 		val ttsMsg = info.ttsMessage
-		val ignoreStrings = prefs!!.getString(getString(R.string.key_ignore_strings), "")!!.lowercase().split("\n").toTypedArray()
+		val ignoreStrings = prefs.getString(getString(R.string.key_ignore_strings), "")!!.lowercase().split("\n").toTypedArray()
 		var stringIgnored = false
 		for (s in ignoreStrings) {
 			if (s.isNotEmpty() && ttsMsg != null && ttsMsg.lowercase().contains(s)) {
@@ -145,11 +166,11 @@ class Service : NotificationListenerService() {
 			info.addIgnoreReason(IgnoreReason.STRING)
 		}
 		if (TextUtils.isEmpty(ttsMsg)
-			&& prefs!!.getBoolean(getString(R.string.key_ignore_empty), false)) {
+			&& prefs.getBoolean(getString(R.string.key_ignore_empty), false)) {
 			info.addIgnoreReason(IgnoreReason.EMPTY_MSG)
 		}
 		var ignoreRepeat = -1
-		val ignoreRepeatStr = prefs!!.getString(getString(R.string.key_ignore_repeat), null)
+		val ignoreRepeatStr = prefs.getString(getString(R.string.key_ignore_repeat), null)
 		if (!TextUtils.isEmpty(ignoreRepeatStr)) {
 			ignoreRepeat = ignoreRepeatStr!!.toInt()
 		}
@@ -161,13 +182,13 @@ class Service : NotificationListenerService() {
 		addNotification(info)
 		if (info.getIgnoreReasons().isEmpty()) {
 			var delay = 0
-			val delayStr = prefs!!.getString(getString(R.string.key_ttsDelay), null)
+			val delayStr = prefs.getString(getString(R.string.key_ttsDelay), null)
 			if (!TextUtils.isEmpty(delayStr)) {
 				delay = delayStr!!.toInt()
 			}
 			if (!isScreenOn()) {
 				var interval = 0
-				val intervalStr = prefs!!.getString(getString(R.string.key_tts_repeat), null)
+				val intervalStr = prefs.getString(getString(R.string.key_tts_repeat), null)
 				if (!TextUtils.isEmpty(intervalStr)) {
 					interval = intervalStr!!.toInt()
 				}
@@ -190,7 +211,9 @@ class Service : NotificationListenerService() {
 						info.addIgnoreReasons(ignoreReasons)
 						return
 					}
-					handler.post { speak(info) }
+					CoroutineScope(Dispatchers.Main).launch {
+						speak(info)
+					}
 				}
 			}, delay * 1000L) // A delay of 0 works fine, and means that all speak calls anywhere are running in their own thread and not blocking.
 			lastMsg[app] = ttsMsg
@@ -208,7 +231,7 @@ class Service : NotificationListenerService() {
 	 * @param info The info for the notification to be spoken.
 	 */
 	private fun speak(info: NotificationInfo) {
-		if (mTts == null) {
+		if (tts == null) {
 			Log.w(TAG, "Speak failed due to service destroyed")
 			info.addIgnoreReason(IgnoreReason.SERVICE_STOPPED)
 			refresh()
@@ -217,24 +240,39 @@ class Service : NotificationListenerService() {
 		val notificationTime = info.calendar.timeInMillis
 		synchronized(ttsQueue) {
 			if (ttsQueue.isEmpty()) { //if there are no messages in the queue, start up shake detection and audio focus requesting
-				shake!!.enable()
+				shake.enable()
 				shouldRequestFocus = getPrefs(applicationContext)
 					.getBoolean(getString(R.string.key_audio_focus), false)
 				if (shouldRequestFocus) {
-					audioMan!!.requestAudioFocus(null, AudioManager.STREAM_MUSIC,
-						AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK)
+					if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+						audioMan.requestAudioFocus(audioFocusRequest!!)
+					} else {
+						@Suppress("DEPRECATION")
+						audioMan.requestAudioFocus(null, AudioManager.STREAM_MUSIC,
+							AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK)
+					}
 				}
 			}
 			//regardless, add the message to the queue, parallelling the TextToSpeech queue since we can't access it.
 			ttsQueue.put(notificationTime, info)
 		}
 		//once the message is in our queue, send it to the real one with the necessary parameters
-		val ttsParams = HashMap<String, String>()
-		//needed because we want to apply stream changes immediately
-		ttsParams[TextToSpeech.Engine.KEY_PARAM_STREAM] = getSelectedAudioStream(applicationContext).toString()
-		//not used anywhere, but has to be set to get the UtteranceProgressListener to trigger its submethods
-		ttsParams[TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID] = notificationTime.toString()
-		if (mTts!!.speak(info.ttsMessage, TextToSpeech.QUEUE_ADD, ttsParams) != TextToSpeech.SUCCESS) {
+		val audioStream = getSelectedAudioStream(applicationContext)
+		val utteranceId = notificationTime.toString()
+		val isSpeakFailed = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+			val ttsParams = Bundle()
+			ttsParams.putInt(TextToSpeech.Engine.KEY_PARAM_STREAM, audioStream)
+			tts!!.speak(info.ttsMessage, TextToSpeech.QUEUE_ADD, ttsParams, utteranceId)
+		} else {
+			val ttsParams = HashMap<String, String>()
+			//needed because we want to apply stream changes immediately
+			ttsParams[TextToSpeech.Engine.KEY_PARAM_STREAM] = audioStream.toString()
+			//not used anywhere, but has to be set to get the UtteranceProgressListener to trigger its submethods
+			ttsParams[TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID] = utteranceId
+			@Suppress("DEPRECATION")
+			tts!!.speak(info.ttsMessage, TextToSpeech.QUEUE_ADD, ttsParams)
+		} != TextToSpeech.SUCCESS
+		if (isSpeakFailed) {
 			Log.e(TAG, "Error adding notification to TTS queue")
 			synchronized(ttsQueue) { ttsQueue.remove(notificationTime) }
 		}
@@ -251,30 +289,39 @@ class Service : NotificationListenerService() {
 		}
 		val c = Calendar.getInstance()
 		val calTime = c[Calendar.HOUR_OF_DAY] * 60 + c[Calendar.MINUTE]
-		val quietStart = prefs!!.getInt(getString(R.string.key_quietStart), 0)
-		val quietEnd = prefs!!.getInt(getString(R.string.key_quietEnd), 0)
+		val quietStart = prefs.getInt(getString(R.string.key_quietStart), 0)
+		val quietEnd = prefs.getInt(getString(R.string.key_quietEnd), 0)
 		if ((quietStart < quietEnd) and (quietStart <= calTime) and (calTime < quietEnd)
 			|| quietEnd < quietStart && (quietStart <= calTime || calTime < quietEnd)) {
 			ignoreReasons.add(IgnoreReason.QUIET)
 		}
-		if ((audioMan!!.ringerMode == AudioManager.RINGER_MODE_SILENT
-				|| audioMan!!.ringerMode == AudioManager.RINGER_MODE_VIBRATE)
-			&& !prefs!!.getBoolean(Common.KEY_SPEAK_SILENT_ON, false)) {
+		if ((audioMan.ringerMode == AudioManager.RINGER_MODE_SILENT
+				|| audioMan.ringerMode == AudioManager.RINGER_MODE_VIBRATE)
+			&& !prefs.getBoolean(Common.KEY_SPEAK_SILENT_ON, false)) {
 			ignoreReasons.add(IgnoreReason.SILENT)
 		}
-		if (telephony!!.callState == TelephonyManager.CALL_STATE_OFFHOOK) {
+		val isInCall = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+			val telecomMan = getSystemService(TELECOM_SERVICE) as TelecomManager
+			if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
+				== PackageManager.PERMISSION_GRANTED
+			) telecomMan.isInCall else false
+		} else {
+			@Suppress("DEPRECATION")
+			telephony.callState == TelephonyManager.CALL_STATE_OFFHOOK
+		}
+		if (isInCall) {
 			ignoreReasons.add(IgnoreReason.CALL)
 		}
-		if (!isScreenOn() && !prefs!!.getBoolean(Common.KEY_SPEAK_SCREEN_OFF, true)) {
+		if (!isScreenOn() && !prefs.getBoolean(Common.KEY_SPEAK_SCREEN_OFF, true)) {
 			ignoreReasons.add(IgnoreReason.SCREEN_OFF)
 		}
-		if (isScreenOn() && !prefs!!.getBoolean(Common.KEY_SPEAK_SCREEN_ON, true)) {
+		if (isScreenOn() && !prefs.getBoolean(Common.KEY_SPEAK_SCREEN_ON, true)) {
 			ignoreReasons.add(IgnoreReason.SCREEN_ON)
 		}
-		if (!isHeadsetOn() && !prefs!!.getBoolean(Common.KEY_SPEAK_HEADSET_OFF, true)) {
+		if (!isHeadsetOn() && !prefs.getBoolean(Common.KEY_SPEAK_HEADSET_OFF, true)) {
 			ignoreReasons.add(IgnoreReason.HEADSET_OFF)
 		}
-		if (isHeadsetOn() && !prefs!!.getBoolean(Common.KEY_SPEAK_HEADSET_ON, true)) {
+		if (isHeadsetOn() && !prefs.getBoolean(Common.KEY_SPEAK_HEADSET_ON, true)) {
 			ignoreReasons.add(IgnoreReason.HEADSET_ON)
 		}
 		return ignoreReasons
@@ -290,7 +337,7 @@ class Service : NotificationListenerService() {
 
 		override fun run() {
 			if (ignore().isNotEmpty()) return
-			handler.post {
+			CoroutineScope(Dispatchers.Main).launch {
 				synchronized(repeatList) {
 					for (info in repeatList) {
 						speak(info)
@@ -318,7 +365,7 @@ class Service : NotificationListenerService() {
 		filter.addAction(TelephonyManager.ACTION_PHONE_STATE_CHANGED)
 		registerReceiver(stateReceiver, filter)
 		shake = Shake(this)
-		shake!!.setOnShakeListener(object : OnShakeListener {
+		shake.setOnShakeListener(object : OnShakeListener {
 			override fun onShake() {
 				Log.i(TAG, "TTS silenced by shake")
 				synchronized(ttsQueue) {
@@ -326,7 +373,7 @@ class Service : NotificationListenerService() {
 						info.addIgnoreReason(IgnoreReason.SHAKE)
 					}
 				}
-				if (mTts != null) mTts!!.stop()
+				tts?.stop()
 				refresh()
 			}
 		})
@@ -337,7 +384,7 @@ class Service : NotificationListenerService() {
 
 	override fun onUnbind(intent: Intent): Boolean {
 		if (isInitialized) {
-			if (mTts != null) mTts!!.stop()
+			tts?.stop()
 			unregisterReceiver(stateReceiver)
 			setInitialized(false)
 			unregisterOnStatusChangeListener(statusListener)
@@ -346,10 +393,8 @@ class Service : NotificationListenerService() {
 	}
 
 	override fun onDestroy() {
-		if (mTts != null) {
-			mTts!!.shutdown()
-			mTts = null
-		}
+		tts?.shutdown()
+		tts = null
 		super.onDestroy()
 	}
 
@@ -373,18 +418,29 @@ class Service : NotificationListenerService() {
 	}
 
 	private fun isHeadsetOn(): Boolean {
-		return audioMan!!.isBluetoothA2dpOn || audioMan!!.isWiredHeadsetOn
+		return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+			audioMan.getDevices(AudioManager.GET_DEVICES_OUTPUTS).any {
+				it.type == AudioDeviceInfo.TYPE_BLUETOOTH_A2DP || it.type == AudioDeviceInfo.TYPE_WIRED_HEADSET
+			}
+		} else {
+			@Suppress("DEPRECATION")
+			audioMan.isBluetoothA2dpOn || audioMan.isWiredHeadsetOn
+		}
 	}
 
 	private object CheckScreen {
-		private var powerMan: PowerManager? = null
+		private lateinit var powerMan: PowerManager
+
 		fun isScreenOn(c: Context): Boolean {
-			if (powerMan == null) {
+			if (!::powerMan.isInitialized) {
 				powerMan = c.getSystemService(POWER_SERVICE) as PowerManager
 			}
-			assert(powerMan != null // Prevent Lint warning. Should never be null, I want a crash report if it is.
-			)
-			return powerMan!!.isScreenOn
+			return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT_WATCH) {
+				powerMan.isInteractive
+			} else {
+				@Suppress("DEPRECATION")
+				powerMan.isScreenOn
+			}
 		}
 	}
 
@@ -392,8 +448,6 @@ class Service : NotificationListenerService() {
 		override fun onReceive(context: Context, intent: Intent) {
 			val action = intent.action
 			var interruptIfIgnored = true
-			assert(action != null // Prevent Lint warning. Should never be null, I want a crash report if it is.
-			)
 			when (action) {
 				Intent.ACTION_SCREEN_ON -> {
 					isScreenOn = true
@@ -408,7 +462,7 @@ class Service : NotificationListenerService() {
 					interruptIfIgnored = false
 				}
 			}
-			if (interruptIfIgnored && mTts != null) {
+			if (interruptIfIgnored && tts != null) {
 				val ignoreReasons = ignore()
 				if (ignoreReasons.isNotEmpty()) {
 					Log.i(TAG, "Notifications silenced/ignored for reason(s): "
@@ -418,7 +472,7 @@ class Service : NotificationListenerService() {
 							info.addIgnoreReasons(ignoreReasons)
 						}
 					}
-					mTts!!.stop()
+					tts!!.stop()
 				}
 			}
 		}

app/src/main/java/com/pilot51/voicenotify/Shake.kt
@@ -24,14 +24,17 @@ import com.pilot51.voicenotify.Common.getPrefs
 import kotlin.math.abs
 import kotlin.math.sqrt
 
-internal class Shake(private val context: Context) : SensorEventListener {
-	private val manager: SensorManager?
-	private val sensor: Sensor
+class Shake(
+	private val context: Context
+) : SensorEventListener {
+	private val manager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+	private val sensor = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
 	private var listener: OnShakeListener? = null
 	private var threshold = 0
 	private var overThresholdCount = 0
 	private var accelCurrent = 0f
 	private var accelLast = 0f
+
 	fun enable() {
 		if (listener == null) return
 		threshold = try {
@@ -40,11 +43,11 @@ internal class Shake(private val context: Context) : SensorEventListener {
 			// Don't enable if threshold setting is blank
 			return
 		}
-		manager!!.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+		manager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
 	}
 
 	fun disable() {
-		manager!!.unregisterListener(this)
+		manager.unregisterListener(this)
 		accelCurrent = 0f
 		accelLast = 0f
 		overThresholdCount = 0
@@ -55,6 +58,7 @@ internal class Shake(private val context: Context) : SensorEventListener {
 	}
 
 	override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+
 	override fun onSensorChanged(event: SensorEvent) {
 		val x = event.values[0]
 		val y = event.values[1]
@@ -72,12 +76,7 @@ internal class Shake(private val context: Context) : SensorEventListener {
 		accelLast = accelCurrent
 	}
 
-	internal interface OnShakeListener {
+	interface OnShakeListener {
 		fun onShake()
-	}
-
-	init {
-		manager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
-		sensor = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
 	}
 }

app/build.gradle.kts
@@ -72,4 +72,5 @@ android {
 
 dependencies {
 	implementation("androidx.core:core-ktx:1.7.0")
+	implementation("androidx.preference:preference-ktx:1.1.1")
 }

app/src/main/AndroidManifest.xml
@@ -39,10 +39,6 @@
 				<category android:name="android.intent.category.LAUNCHER" />
 			</intent-filter>
 		</activity>
-		<activity
-			android:name=".AppListActivity"
-			android:configChanges="keyboardHidden|orientation">
-		</activity>
 		<service
 			android:name=".Service"
 			android:permission="android.permission.BIND_NOTIFICATION_LISTENER_SERVICE"

app/src/main/java/com/pilot51/voicenotify/App.kt
@@ -15,7 +15,11 @@
  */
 package com.pilot51.voicenotify
 
-class App(val `package`: String, val label: String, var enabled: Boolean) {
+class App(
+	val `package`: String,
+	val label: String,
+	var enabled: Boolean
+) {
 	/**
 	 * Updates self in database.
 	 * @return This instance.

commit 2c05f81fceda55bf9a722d4f386d0bbcd98c5f2e

Prevent NFE crashes, support decimal values for some preferences
- Regression at 870b1a3e caused by bug in Jetpack Preference:
  https://issuetracker.google.com/issues/37060038

Decimal support: TTS Delay, TTS Repeat, Shake-To-Silence

app/src/main/java/com/pilot51/voicenotify/NotificationInfo.kt
@@ -106,12 +106,14 @@ class NotificationInfo @SuppressLint("InlinedApi") constructor(
 			}
 		}
 		if (ttsMessage != null) {
-			val maxLengthStr = prefs.getString(appContext.getString(R.string.key_max_length), null)
-			if (!maxLengthStr.isNullOrEmpty()) {
-				val maxLength = maxLengthStr.toInt()
+			try {
+				val maxLength = prefs.getString(appContext.getString(R.string.key_max_length), null)
+					?.takeIf { it.isNotEmpty() }?.toInt() ?: 0
 				if (maxLength > 0) {
 					ttsMessage = ttsMessage!!.substring(0, min(maxLength, ttsMessage!!.length))
 				}
+			} catch (e: NumberFormatException) {
+				Log.w(TAG, "Failed to parse Maximum Message: ${e.message}")
 			}
 		}
 	}
@@ -189,5 +191,9 @@ class NotificationInfo @SuppressLint("InlinedApi") constructor(
 	 */
 	fun getIgnoreReasons(): Set<IgnoreReason> {
 		return ignoreReasons
+	}
+
+	companion object {
+		private val TAG = NotificationInfo::class.simpleName
 	}
 }

app/src/main/java/com/pilot51/voicenotify/Service.kt
@@ -175,10 +175,12 @@ class Service : NotificationListenerService() {
 				info.addIgnoreReason(IgnoreReason.STRING_IGNORED)
 			}
 		}
-		var ignoreRepeat = -1
-		val ignoreRepeatStr = prefs.getString(getString(R.string.key_ignore_repeat), null)
-		if (!ignoreRepeatStr.isNullOrEmpty()) {
-			ignoreRepeat = ignoreRepeatStr.toInt()
+		val ignoreRepeat = try {
+			prefs.getString(getString(R.string.key_ignore_repeat), null)
+				?.takeIf { it.isNotEmpty() }?.toInt() ?: -1
+		} catch (e: NumberFormatException) {
+			Log.w(TAG, "Failed to parse Ignore Repeats: ${e.message}")
+			-1
 		}
 		if (lastMsg.containsKey(app)) {
 			if (lastMsg[app] == ttsMsg && (ignoreRepeat == -1 || msgTime - lastMsgTime[app]!! < ignoreRepeat * 1000)) {
@@ -187,16 +189,20 @@ class Service : NotificationListenerService() {
 		}
 		addNotification(info)
 		if (info.getIgnoreReasons().isEmpty()) {
-			var delay = 0
-			val delayStr = prefs.getString(getString(R.string.key_ttsDelay), null)
-			if (!delayStr.isNullOrEmpty()) {
-				delay = delayStr.toInt()
+			val delay = try {
+				prefs.getString(getString(R.string.key_ttsDelay), null)
+					?.takeIf { it.isNotEmpty() }?.toDouble()?.takeUnless { it < 0.0 } ?: 0.0
+			} catch (e: NumberFormatException) {
+				Log.w(TAG, "Failed to parse TTS Delay: ${e.message}")
+				0.0
 			}
 			if (!isScreenOn()) {
-				var interval = 0
-				val intervalStr = prefs.getString(getString(R.string.key_tts_repeat), null)
-				if (!intervalStr.isNullOrEmpty()) {
-					interval = intervalStr.toInt()
+				val interval = try {
+					prefs.getString(getString(R.string.key_tts_repeat), null)
+						?.takeIf { it.isNotEmpty() }?.toDouble() ?: 0.0
+				} catch (e: NumberFormatException) {
+					Log.w(TAG, "Failed to parse TTS Repeat: ${e.message}")
+					0.0
 				}
 				if (interval > 0) {
 					synchronized(repeatList) { repeatList.add(info) }
@@ -204,9 +210,6 @@ class Service : NotificationListenerService() {
 						repeater = RepeatTimer(interval)
 					}
 				}
-			}
-			if (delay < 0) { // Just in case we get a weird value, don't want to try to make the Timer wait for negative time
-				delay = 0
 			}
 			Timer().schedule(object : TimerTask() {
 				override fun run() {
@@ -221,7 +224,7 @@ class Service : NotificationListenerService() {
 						speak(info)
 					}
 				}
-			}, delay * 1000L) // A delay of 0 works fine, and means that all speak calls anywhere are running in their own thread and not blocking.
+			}, (delay * 1000).toLong()) // A delay of 0 works fine, and means that all speak calls anywhere are running in their own thread and not blocking.
 			lastMsg[app] = ttsMsg
 			lastMsgTime[app] = msgTime
 		} else {
@@ -332,10 +335,10 @@ class Service : NotificationListenerService() {
 		return ignoreReasons
 	}
 
-	private inner class RepeatTimer(minuteInterval: Int) : TimerTask() {
+	private inner class RepeatTimer(minuteInterval: Double) : TimerTask() {
 		init {
 			if (minuteInterval > 0) {
-				val interval = minuteInterval * 60000L
+				val interval = (minuteInterval * 60000).toLong()
 				Timer().schedule(this, interval, interval)
 			}
 		}

app/src/main/java/com/pilot51/voicenotify/Shake.kt
@@ -20,6 +20,7 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.util.Log
 import com.pilot51.voicenotify.Common.prefs
 import com.pilot51.voicenotify.VNApplication.Companion.appContext
 import kotlin.math.abs
@@ -29,7 +30,7 @@ class Shake : SensorEventListener {
 	private val manager = appContext.getSystemService(SENSOR_SERVICE) as SensorManager
 	private val sensor = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
 	var onShake: (() -> Unit)? = null
-	private var threshold = 0
+	private var threshold = 0.0
 	private var overThresholdCount = 0
 	private var accelCurrent = 0f
 	private var accelLast = 0f
@@ -37,9 +38,10 @@ class Shake : SensorEventListener {
 	fun enable() {
 		if (onShake == null) return
 		threshold = try {
-			prefs.getString(appContext.getString(R.string.key_shake_threshold), null)!!.toInt()
+			prefs.getString(appContext.getString(R.string.key_shake_threshold), null)
+				?.takeIf { it.isNotEmpty() }?.toDouble() ?: return
 		} catch (e: NumberFormatException) {
-			// Don't enable if threshold setting is blank
+			Log.w(TAG, "Failed to parse shake threshold: ${e.message}")
 			return
 		}
 		manager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
@@ -69,5 +71,9 @@ class Shake : SensorEventListener {
 			overThresholdCount = 0
 		}
 		accelLast = accelCurrent
+	}
+
+	companion object {
+		private val TAG = Shake::class.simpleName
 	}
 }