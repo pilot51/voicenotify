44ca3338dc86d3d58da52c9045f8492058f52bae
Terry Cai <gidcai@gmail.com>
2024-05-21 09:57:28 +0800

    fix

--- app/src/main/java/com/pilot51/voicenotify/ui/Modifier.kt
+++ app/src/main/java/com/pilot51/voicenotify/ui/Modifier.kt
@@ -26,3 +26,20 @@ fun Modifier.bottomBorder(strokeWidth: Dp, color: Color) = composed(
     }
 )
 
+
+fun Modifier.topBorder(strokeWidth: Dp, color: Color) = composed(
+    factory = {
+        val density = LocalDensity.current
+        val strokeWidthPx = density.run { strokeWidth.toPx() }
+        Modifier.drawBehind {
+            val width = size.width
+            val height = 0f +  Math.round(-strokeWidthPx/2)
+            drawLine(
+                color = color,
+                start = Offset(x = 0f, y = height),
+                end = Offset(x = width , y = height),
+                strokeWidth = strokeWidthPx
+            )
+        }
+    }
+)
