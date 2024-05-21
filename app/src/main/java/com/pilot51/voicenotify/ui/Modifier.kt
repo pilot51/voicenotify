7a6f0a7b4bab16337417ccbb26c182e1ce0617fe
Terry Cai <gidcai@gmail.com>
2024-05-21 11:41:24 +0800

    feat: Modifier fun

--- app/src/main/java/com/pilot51/voicenotify/ui/Modifier.kt
+++ app/src/main/java/com/pilot51/voicenotify/ui/Modifier.kt
@@ -1,12 +1,19 @@
 package com.pilot51.voicenotify.ui
 
+import androidx.compose.foundation.border
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.layout.onPlaced
+import androidx.compose.ui.unit.dp
 
 
 fun Modifier.bottomBorder(strokeWidth: Dp, color: Color) = composed(
@@ -14,6 +21,7 @@ fun Modifier.bottomBorder(strokeWidth: Dp, color: Color) = composed(
         val density = LocalDensity.current
         val strokeWidthPx = density.run { strokeWidth.toPx() }
         Modifier.drawBehind {
+        //  val strokeWidth = Dp.Hairline.toPx()
             val width = size.width
             val height = size.height + Math.round(-strokeWidthPx/2)
             drawLine(
@@ -43,3 +51,24 @@ fun Modifier.topBorder(strokeWidth: Dp, color: Color) = composed(
         }
     }
 )
+
+
+inline fun Modifier.addIf(
+    predicate: Boolean,
+    crossinline whenTrue: @Composable () -> Modifier,
+): Modifier = composed {
+    if (predicate) {
+        this.then(whenTrue())
+    } else {
+        this
+    }
+}
+
+fun Modifier.measured(block: (DpSize) -> Unit): Modifier = composed {
+    val density = LocalDensity.current
+    onPlaced { block(it.size.toDp(density)) }
+}
+
+fun Modifier.debugBounds(color: Color = Color.Magenta, shape: Shape = RectangleShape) =
+    this.border(1.dp, color, shape)
+
