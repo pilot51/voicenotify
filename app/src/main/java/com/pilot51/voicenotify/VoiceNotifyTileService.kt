af7d553c22dcfed12b05521265fd7df4383b27d5
Alex Balgavy <8124851+thezeroalpha@users.noreply.github.com>
2024-09-21 22:27:55 +0200

    Add a quick settings tile to toggle Voice Notify (#140)
    Add a quick settings tile to toggle Voice Notify
    
    Adds a quick settings tile (TileService) that displays the current state
    of Voice Notify. Clicking on the tile toggles Voice Notify.
    Long-pressing the tile opens the main screen of Voice Notify.

---
+++ app/src/main/java/com/pilot51/voicenotify/VoiceNotifyTileService.kt
@@ -0,0 +1,33 @@
+package com.pilot51.voicenotify
+
+import android.os.Build
+import android.service.quicksettings.Tile
+import android.service.quicksettings.TileService
+import androidx.annotation.RequiresApi
+
+@RequiresApi(Build.VERSION_CODES.N)
+class VoiceNotifyTileService : TileService() {
+    // Called when the tile can be updated
+    override fun onStartListening() {
+        super.onStartListening()
+        updateTile(Service.isSuspended.value)
+    }
+
+    // Called when the user taps on the tile in an active or inactive state.
+    override fun onClick() {
+        super.onClick()
+        val isSuspended = Service.toggleSuspend()
+        updateTile(isSuspended)
+    }
+
+    private fun updateTile(suspended: Boolean) {
+        val isRunning = Service.isRunning.value
+
+        qsTile.state = when {
+            !isRunning -> Tile.STATE_UNAVAILABLE
+            isRunning && suspended -> Tile.STATE_INACTIVE
+            else -> Tile.STATE_ACTIVE
+        }
+        qsTile.updateTile()
+    }
+}
